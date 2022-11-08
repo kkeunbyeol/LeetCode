a0d97879a46fd442906b5c7b4c15b3c9966bf244
kkeunbyeol <86813592+kkeunbyeol@users.noreply.github.com>
2022-11-08 10:29:25 +0900

    Time: 2054 ms (55.82%), Space: 17.1 MB (95.18%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,35 @@
+import itertools
+
+class Solution:
+#     def threeSum(self, nums: List[int]) -> List[List[int]]:
+#         answer = []
+#         duplicates = set()
+#         num_sets = itertools.combinations(nums,3)
+#         for num_set in num_sets:
+#             if sum(num_set)==0:
+#                 if not set(num_set) in duplicates:
+#                     answer.append(num_set)
+#                     duplicates.add(frozenset(num_set))
+#         return answer
+    
+    def threeSum(self, nums):
+        res = []
+        nums.sort()
+        for i in range(len(nums)-2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            l, r = i+1, len(nums)-1
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s < 0:
+                    l +=1 
+                elif s > 0:
+                    r -= 1
+                else:
+                    res.append((nums[i], nums[l], nums[r]))
+                    while l < r and nums[l] == nums[l+1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r-1]:
+                        r -= 1
+                    l += 1; r -= 1
+        return res
